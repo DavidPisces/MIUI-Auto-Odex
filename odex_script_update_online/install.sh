3495be50e76fec5c5b83192550d718f9d54d2ae3
柚稚的孩纸 <zjw2017@users.noreply.github.com>
2022-02-02 00:30:06 +0800

    新增 更新系统后自动删除上一次的odex结果，支持Magisk 24.0在线更新脚本

---
+++ odex_script_update_online/install.sh
@@ -0,0 +1,24 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=true
+LATESTARTSERVICE=false
+REPLACE="
+"
+on_install(){
+  ui_print "- 提取模块文件"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+print_modname() {
+ui_print "**************************************************************"
+ui_print "- MIUI ODEX 脚本更新模块"
+ui_print "- Made By 柚稚的孩纸&雄氏老方"
+ui_print "**************************************************************"
+}
+touch $TMPDIR/now_version
+modelversion="$(getprop ro.system.build.version.incremental)"
+echo "$modelversion" >>$TMPDIR/now_version
+rm -rf /storage/emulated/0/MIUI_odex/odex.sh
+rm -rf /storage/emulated/0/MIUI_odex/odex.json
+mv $TMPDIR/odex.sh /storage/emulated/0/MIUI_odex/odex.sh
+mv $TMPDIR/odex.json /storage/emulated/0/MIUI_odex/odex.json
+echo -n "description=$(cat $TMPDIR/odex.md | sed '1d')">>$TMPDIR/module.prop
