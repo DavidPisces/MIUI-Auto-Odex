68cb88a72b09d0931f47f672b5330dd983319913
柚稚的孩纸 <zjw2017@users.noreply.github.com>
2022-02-02 01:02:22 +0800

    新增 更新系统后自动删除上一次的odex结果，支持Magisk 24.0在线更新脚本

--- odex_script_update_online/common/post-fs-data.sh
+++ odex_script_update_online/common/post-fs-data.sh
@@ -2,5 +2,5 @@ MODDIR=${0%/*}
 new_version="$(getprop ro.system.build.version.incremental)"
 preview_version="$(cat $MODDIR/now_version)"
 if [ $new_version != $preview_version ]; then
-    rm -rf /data/adb/modules/miuiodex
+    touch /data/adb/modules/miuiodex/disable
 fi
