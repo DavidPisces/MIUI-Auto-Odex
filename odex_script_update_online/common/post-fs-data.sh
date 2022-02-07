350fa08efec01f6418f1a852401e5bed3cf21237
柚稚的孩纸 <1987836456+zjw2017@users.noreply.github.com>
2022-02-07 07:37:31 +0000

    新增 自动打包后，自动提交下载链接

--- odex_script_update_online/common/post-fs-data.sh
+++
@@ -1,6 +0,0 @@
-MODDIR=${0%/*}
-new_version="$(getprop ro.system.build.version.incremental)"
-preview_version="$(cat $MODDIR/now_version)"
-if [ $new_version != $preview_version ]; then
-    touch /data/adb/modules/miuiodex/disable
-fi
