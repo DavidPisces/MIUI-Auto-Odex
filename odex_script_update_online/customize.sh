d8a9ecc58e514081b6c5c56300da46e9f04da1e6
柚稚的孩纸 <30484319+zjw2017@users.noreply.github.com>
2023-03-01 17:39:16 +0800

    对脚本进行了大量优化，简化了根据包名进行编译，适配MIUI14和安卓13，对精简模式添加了大量系统高频应用

--- odex_script_update_online/customize.sh
+++ odex_script_update_online/customize.sh
@@ -13,13 +13,12 @@ if [ "$MAGISK_VER_CODE" -lt 24000 ]; then
   ui_print "! Please install Magisk 24.0+"
   abort "*********************************************************"
 fi
-if [ ! -d /storage/emulated/0/MIUI_odex ]; then
-  mkdir -p /storage/emulated/0/MIUI_odex
+if [ ! -d /storage/emulated/0/Android/MIUI_odex ]; then
+  mkdir -p /storage/emulated/0/Android/MIUI_odex
 else
-  [ -f /storage/emulated/0/MIUI_odex/odex.sh ] && rm -rf /storage/emulated/0/MIUI_odex/odex.sh
-  [ -f /storage/emulated/0/MIUI_odex/odex.json ] && rm -rf /storage/emulated/0/MIUI_odex/odex.json
+  [ -f /storage/emulated/0/Android/MIUI_odex/odex.sh ] && rm -rf /storage/emulated/0/Android/MIUI_odex/odex.sh
+  [ -f /storage/emulated/0/Android/MIUI_odex/odex.json ] && rm -rf /storage/emulated/0/Android/MIUI_odex/odex.json
 fi
-cp -f "$MODPATH"/odex.sh /storage/emulated/0/MIUI_odex && rm -rf "$MODPATH"/odex.sh
-[ ! -f /storage/emulated/0/MIUI_odex/Simple_List.prop ] && cp -f "$MODPATH"/Simple_List.prop /storage/emulated/0/MIUI_odex
+cp -f "$MODPATH"/odex.sh /storage/emulated/0/Android/MIUI_odex && rm -rf "$MODPATH"/odex.sh
+[ ! -f /storage/emulated/0/Android/MIUI_odex/Simple_List.prop ] && cp -f "$MODPATH"/Simple_List.prop /storage/emulated/0/Android/MIUI_odex
 rm -rf "$MODPATH"/Simple_List.prop
-rm -rf "$MODPATH"/system.prop
